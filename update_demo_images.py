398f0e3e3124e10caf0ceb1a7e971491c4174ef2
Juan Carlos Bardoneschi <jcbardoneschi@hotmail.com>
2025-12-01 12:46:52 -0300

    Fix demo pages: usar base64 real desde BD (no URLs Cloudinary)

--- update_demo_images.py
+++ update_demo_images.py
@@ -9,18 +9,33 @@ demo_html_path = 'clip_admin_backend/app/static/demo-store.html'
 with open(demo_html_path, 'r', encoding='utf-8') as f:
     demo_html = f.read()
 
-# Reemplazar URLs por base64
-replacements = {
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod1': images['GOD-CHQ-002'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod2': images['GOD-CAM-001'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod3': images['GOD-GOR-001'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod4': images['GOD-CAR-002'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod5': images['GOD-BUZ-001'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod6': images['GOD-AMB-002'],
-}
+# Reemplazar URLs por base64 - usar regex simple reemplazando cualquier cloudinary URL con los base64 en orden
+import re
 
-for url, base64 in replacements.items():
-    demo_html = demo_html.replace(url, base64)
+# Buscar todas las URLs cloudinary en orden
+cloudinary_pattern = r'https://res\.cloudinary\.com/[^"\']+clients_demo_fashion_store_[^"\']+'
+
+# Las imágenes en orden según aparecen en HTML
+base64_order = [
+    images['GOD-CHQ-002'],  # Chaqueta Negra
+    images['GOD-CAM-001'],  # Camisa Blanca
+    images['GOD-GOR-001'],  # Boina Calada
+    images['GOD-CAR-002'],  # Cardigan Dama
+    images['GOD-BUZ-001'],  # Buzo Frizado
+    images['GOD-AMB-002'],  # Ambo New Goody
+]
+
+def replace_nth_cloudinary(match):
+    """Reemplaza cada URL encontrada con el siguiente base64 de la lista"""
+    idx = replace_nth_cloudinary.counter
+    replace_nth_cloudinary.counter += 1
+    if idx < len(base64_order):
+        return base64_order[idx]
+    return match.group(0)
+
+replace_nth_cloudinary.counter = 0
+
+demo_html = re.sub(cloudinary_pattern, replace_nth_cloudinary, demo_html)
 
 with open(demo_html_path, 'w', encoding='utf-8') as f:
     f.write(demo_html)
@@ -32,8 +47,8 @@ eve_html_path = 'clip_admin_backend/app/static/eve-store.html'
 with open(eve_html_path, 'r', encoding='utf-8') as f:
     eve_html = f.read()
 
-for url, base64 in replacements.items():
-    eve_html = eve_html.replace(url, base64)
+replace_nth_cloudinary.counter = 0
+eve_html = re.sub(cloudinary_pattern, replace_nth_cloudinary, eve_html)
 
 with open(eve_html_path, 'w', encoding='utf-8') as f:
     f.write(eve_html)
@@ -45,17 +60,43 @@ noa_html_path = 'clip_admin_backend/app/static/noa-store.html'
 with open(noa_html_path, 'r', encoding='utf-8') as f:
     noa_html = f.read()
 
-noa_replacements = {
-    'https://res.cloudinary.com/dxvprqsf8/image/upload/v1/clip_v2/clip_v2/clients_demo_noa_prod1': images['PKHT-001-S'],
-    'https://res.cloudinary.com/dxvprqsf8/image/upload/v1/clip_v2/clip_v2/clients_demo_noa_prod2': images['CHKR-STR-002-N'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod3': images['GOD-GOR-001'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod4': images['GOD-CAR-002'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod5': images['GOD-BUZ-001'],
-    'https://res.cloudinary.com/dgtsan81n/image/upload/v1/clip_v2/clip_v2/clients_demo_fashion_store_prod6': images['GOD-AMB-002'],
-}
+# Patrones para Noa Store (2 productos Noa primero, luego 4 Goody)
+noa_cloudinary_pattern = r'https://res\.cloudinary\.com/[^"\']+clients_demo_noa_[^"\']+'
+goody_cloudinary_pattern = r'https://res\.cloudinary\.com/[^"\']+clients_demo_fashion_store_[^"\']+'
 
-for url, base64 in noa_replacements.items():
-    noa_html = noa_html.replace(url, base64)
+# Primero reemplazar las 2 de Noa
+noa_base64_order = [
+    images['PKHT-001-S'],    # Colgante Silver Heart
+    images['CHKR-STR-002-N'], # Choker Black Star
+]
+
+def replace_nth_noa(match):
+    idx = replace_nth_noa.counter
+    replace_nth_noa.counter += 1
+    if idx < len(noa_base64_order):
+        return noa_base64_order[idx]
+    return match.group(0)
+
+replace_nth_noa.counter = 0
+noa_html = re.sub(noa_cloudinary_pattern, replace_nth_noa, noa_html)
+
+# Luego reemplazar las 4 de Goody (reutilizar primeras 4)
+goody_base64_for_noa = [
+    images['GOD-GOR-001'],  # Boina
+    images['GOD-CAR-002'],  # Cardigan
+    images['GOD-BUZ-001'],  # Buzo
+    images['GOD-AMB-002'],  # Ambo
+]
+
+def replace_nth_goody_in_noa(match):
+    idx = replace_nth_goody_in_noa.counter
+    replace_nth_goody_in_noa.counter += 1
+    if idx < len(goody_base64_for_noa):
+        return goody_base64_for_noa[idx]
+    return match.group(0)
+
+replace_nth_goody_in_noa.counter = 0
+noa_html = re.sub(goody_cloudinary_pattern, replace_nth_goody_in_noa, noa_html)
 
 with open(noa_html_path, 'w', encoding='utf-8') as f:
     f.write(noa_html)
